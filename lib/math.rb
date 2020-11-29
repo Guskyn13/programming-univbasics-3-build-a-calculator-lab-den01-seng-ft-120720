66c17639a3e44ad8f441faf45f52854fed8bf591
Nathan K Gusky <n.gusky88@gmail.com>
2020-11-29 09:41:11 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,21 +3,22 @@ sum = num2 + num1
 end
 addition(5,4)
 def subtraction(num1, num2)
-
+subtraction = num2 - num1
 end
-
+subtraction(10,5)
 def division(num1, num2)
-
+division = num2 / num1
 end
-
+division(50,2)
 def multiplication(num1, num2)
-
+  multiplication = num2 * num1
 end
-
+multiplcation(4,30)
 def modulo(num1, num2)
-
+  modulo = num2 % num1
 end
-
+modulo(34,5)
 def square_root(num)
-
+  square_root = num2 ** num1
 end
+square_root(81)
